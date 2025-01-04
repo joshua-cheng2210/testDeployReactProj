2ef564a717bb99a521cdeb3822a1ffa8d41f850e
chen7647 <chen7647@umn.edu>
2025-01-04 14:43:19 -0600

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   build: {
-    base: './', // Ensure the base path is set correctly for GitHub Pages
+    base: './testDeployReactProj', // Ensure the base path is set correctly for GitHub Pages
     outDir: 'dist',
     rollupOptions: {
       input: {
