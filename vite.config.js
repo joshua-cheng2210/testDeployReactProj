281c5437e6edd54b279a48e372088cd37874924b
chen7647 <chen7647@umn.edu>
2025-01-03 18:32:04 -0600

    init

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    base: './',
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      },
+      output: {
+        entryFileNames: 'assets/index.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
+});
